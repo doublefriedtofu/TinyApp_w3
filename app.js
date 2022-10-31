63f4cb4dde930e38022f70b10f09673a260a031f
doublefriedtofu <hurdonghyuk@gmail.com>
2022-10-31 15:55:08 -0400

    fixed with answers :(

--- app.js
+++ app.js
@@ -1,21 +1,15 @@
-const { response } = require("express");
 const http = require("http");
 const PORT = 8080;
 
 // a function whcih handles request and sends response
-const requestHandler = ( request, response ) => {
-  console.log("In requestHandler");
-  const responseText = `${request.url} ${request.method}`;
-  
-  switch (responseText) {
-    case 'GET /':
-      response.end("Welcome to the home page");
-      break;
-    case 'GET /urls':
-      response.end(request.url)
-      break;
-    default:
-      response.end(`${response.statusCode} Page Not Found`);
+const requestHandler = ( request, response ) => {  
+  if (request.url === "/") {
+    response.end("Welcome to the home page!");
+  } else if (request.url === "/urls") {
+    response.end("www.lighthouselabs.ca\nwww.google.com");
+  } else {
+    response.statusCode = 404;
+    response.end("404 Page Not Found");
   }
 };
 
@@ -27,5 +21,3 @@ console.log("Server created");
 server.listen(PORT, () => {
   console.log(`Server listening on: http://localhost:${PORT}`);
 });
-
-console.log("Last line (after .listen call)");
