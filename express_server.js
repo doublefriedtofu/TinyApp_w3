4a19bb6f1af4ce1459162fba5f7d0579010b1e19
doublefriedtofu <hurdonghyuk@gmail.com>
2022-10-31 21:05:54 -0400

    added a comment about /urls/:id

--- express_server.js
+++ express_server.js
@@ -32,6 +32,7 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+// added a another route for /urls/:id; ":" tells that id is a route parameter
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
