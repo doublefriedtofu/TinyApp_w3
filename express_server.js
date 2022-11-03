0dec1db48e30a17894ba02a51924be03e45b3eee
doublefriedtofu <hurdonghyuk@gmail.com>
2022-11-03 19:04:14 -0400

    changed the name of the cookie value; made all features dont work when not logged in expect when sharing a link.

--- express_server.js
+++ express_server.js
@@ -31,16 +31,16 @@ const users = {
 };
 
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  return res.send("Hello!");
 });
 
 // sends a message to conosle that server is running
 app.listen(PORT, () => {
-  console.log(`Example app listening on port${PORT}!`);
+  return console.log(`Example app listening on port${PORT}!`);
 });
 
 app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+  return res.json(urlDatabase);
 });
 
 // a route for /urls
@@ -51,7 +51,7 @@ app.get("/urls", (req, res) => {
     user: foundUserInfo,
     urls: urlDatabase
   };
-  res.render("urls_index", templateVars);
+  return res.render("urls_index", templateVars);
 });
 
 // adds a new route(page) to submit long url
@@ -61,7 +61,10 @@ app.get("/urls/new", (req, res) => {
   const templateVars = {
     user: foundUserInfo
   };
-  res.render("urls_new", templateVars);
+  if (!templateVars.user) {
+    return res.redirect("/login");
+    }
+  return res.render("urls_new", templateVars);
 });
 
 // added a another route for /urls/:id; ":" tells that id is a route parameter
@@ -74,14 +77,17 @@ app.get("/urls/:id", (req, res) => {
     user: foundUserInfo,
     urls: urlDatabase
   };
-  res.render("urls_show", templateVars);
+  return res.render("urls_show", templateVars);
 });
 
 // redirects to the longURL page when clicked
 app.get("/u/:id", (req, res) => {
   const shortURL = req.params.id;
   const longURL = urlDatabase[shortURL];
-  res.redirect(longURL);
+  if (shortURL && longURL) {
+    return res.redirect(longURL);
+  }
+  return res.end(`The ${shortURL} does not exist`);
 });
 
 // register route
@@ -89,7 +95,11 @@ app.get("/register", (req, res) => {
   const templateVars = {
     user: req.cookies["user_id"]
   };
-  res.render("register", templateVars);
+    // if the user is logged in, go to /urls otherwise to /register
+  if (templateVars.user) {
+    return res.redirect("/urls");
+    }    
+  return res.render("register", templateVars);
 });
 
 // a login route
@@ -97,13 +107,18 @@ app.get("/login", (req, res) => {
   const templateVars = {
     user: req.cookies["user_id"]
   };
-  res.render("login_page", templateVars);
+  // if the user is logged in, go to /urls otherwise to /login
+  if (templateVars.user) {
+    return res.redirect("/urls");
+    }    
+  return res.render("login_page", templateVars);
 });
 
 // signs in using a cookie
 app.post("/login", (req, res) => {
   const foundUser = findUsers(req.body.email);
   const inputUserPass = req.body.inputPassword;
+  // if user and password is in database, reutrn url otherwise, error
   if (foundUser && foundUser.password === inputUserPass) {
     const userID = foundUser.id;
     res.cookie('user_id', userID);
@@ -114,44 +129,47 @@ app.post("/login", (req, res) => {
 
 // using POST request to handle the "submit" button
 app.post("/urls", (req, res) => {
+  console.log("this:",req.cookies["user_id"])
+  if (!req.cookies["user_id"]) {
+    return res.end('You cannot create new shortened URL if you are not logged in.');
+  }   
+
   // response after a submit button
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
-  res.redirect(`/urls/${shortURL}`);
+  return res.redirect(`/urls/${shortURL}`);
 });
 
 // edits the short URL using POST
 app.post("/urls/:id", (req, res) => {
   const id = req.params.id;
   urlDatabase[id] = req.body.longURL;
-  res.redirect(`/urls/${id}`);
+  return res.redirect(`/urls/${id}`);
 });
 
 // deletes the URL using POST
 app.post("/urls/:id/delete", (req, res) => {
   const id = req.params.id;
   delete urlDatabase[id];
-  res.redirect("/urls");
+  return res.redirect("/urls");
 });
 
 // logout request
 app.post("/logout", (req, res) => {
   res.clearCookie("user_id");
-  res.redirect("/login");
+  return res.redirect("/login");
 });
 /////////////////////////////////////
-// signs in using a cookie
-app.post("/login", (req, res) => {
-  const foundUser = findUsers(req.body.email);
-  const inputUserPass = req.body.inputPassword;
-  if (foundUser && foundUser.password === inputUserPass) {
-    const userID = foundUser.id;
-    res.cookie('user_id', userID);
-    return res.redirect("/urls");
-  }
-  return res.end('<html><head><title>403: Forbidden</title></head><body><h1>403: Forbidden</h1></body></html>');
-});
-
+// app.post("/login", (req, res) => {
+//   const foundUser = findUsers(req.body.email);
+//   const inputUserPass = req.body.inputPassword;
+//   if (foundUser && foundUser.password === inputUserPass) {
+//     const userID = foundUser.id;
+//     res.cookie('user_id', userID);
+//     return res.redirect("/urls");
+//   }
+//   return res.end('<html><head><title>403: Forbidden</title></head><body><h1>403: Forbidden</h1></body></html>');
+// });
 
 app.post("/register", (req, res) => {
   const randomUserID = generateRandomString();
@@ -168,7 +186,7 @@ app.post("/register", (req, res) => {
     users[randomUserID] = newUserInfo;
   }
   res.cookie('user_id', randomUserID);
-  res.redirect("/urls");
+  return res.redirect("/urls");
 });
 
 const generateRandomString = () => {
@@ -195,11 +213,9 @@ const findUsersByID = (userID) => {
 };
 
 const getUserByEmail = (inputInfo, res) => {
-  if (!inputInfo.email || !inputInfo.password) {
-    return res.end('<html><head><title>403: Forbidden</title></head><body><h1>403: Forbidden</h1></body></html>');
-  }
-  if (findUsers(inputInfo.email)) {
+  if (!inputInfo.email || !inputInfo.password || findUsers(inputInfo.email)) {
     return res.end('<html><head><title>403: Forbidden</title></head><body><h1>403: Forbidden</h1></body></html>');
   }
   return;
 };
+
