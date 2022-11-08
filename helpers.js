4a2001de9bd669e04132a2963448533bbc8fb979
doublefriedtofu <hurdonghyuk@gmail.com>
2022-11-08 14:58:44 -0500

    extracted all functions from express_server.js

--- helpers.js
+++ helpers.js
@@ -1,3 +1,16 @@
+
+const generateRandomString = () => {
+  let randomString = "";
+  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  const numbers = "1234567890";
+  const alphaNumbers = alphabet + numbers;
+  randomString += alphabet[Math.floor(Math.random() * alphabet.length)];
+  for (let i = 0; i < 5; i++) {
+    randomString += alphaNumbers[Math.floor(Math.random() * alphaNumbers.length)];
+  }
+  return randomString;
+};
+
 const getUserByEmail = (inputEmail, database) => {
   for (const [key, value] of Object.entries(database)) {
     const usersToCheck = value["email"];
@@ -8,4 +21,28 @@ const getUserByEmail = (inputEmail, database) => {
   return false;
 };
 
-module.exports = { getUserByEmail }
+const findUsersByID = (userData, userID) => {
+  return userData[userID];
+};
+
+const urlsForUser = (urlData,userID) => {
+  const filteredURL = {};
+  for (const shortURL in urlData) {
+    if (urlData[shortURL]["userID"] === userID) {
+      filteredURL[shortURL] = urlData[shortURL];
+    }
+  }
+  return filteredURL;
+};
+
+const equalShortURL = (urlData, shortURL) => {
+  for (const shortURLs in urlData) {
+    if (urlData[shortURLs] === shortURL) {
+      return true;
+    }
+  }
+  return false;
+};
+
+
+module.exports = { getUserByEmail, findUsersByID, urlsForUser, equalShortURL, generateRandomString }
