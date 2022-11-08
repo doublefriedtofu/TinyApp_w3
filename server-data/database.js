bc5c44a0b17bc4347abefb2256adfd54885bec7e
doublefriedtofu <hurdonghyuk@gmail.com>
2022-11-08 14:55:52 -0500

    separated database to a different file

---
+++ server-data/database.js
@@ -0,0 +1,20 @@
+const urlDatabase = {
+  b2xVn2: {
+    longURL: "http://www.lighthouselabs.ca",
+    userID: "user1RandomID"
+  },
+  k3m5xK: {
+    longURL: "http://www.f1google.com",
+    userID: "user2RandomID",
+  },
+  k3msdK: {
+    longURL: "http://www.f2google.com",
+    userID: "user1RandomID",
+  },
+  k3masd: {
+    longURL: "http://www.f3google.com",
+    userID: "user2RandomID",
+  },
+};
+
+module.exports = { urlDatabase };
