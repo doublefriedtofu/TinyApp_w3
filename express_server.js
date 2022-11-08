3acd77ccd2492bc432d1ddd9009fed5125ed251d
doublefriedtofu <hurdonghyuk@gmail.com>
2022-11-08 14:57:20 -0500

    separated userinfo and urlDatabase

--- express_server.js
+++ express_server.js
@@ -3,7 +3,9 @@ const express = require("express");
 const bcrypt = require("bcryptjs");
 const app = express();
 const PORT = 8080;
-const { getUserByEmail } = require("./helpers.js");
+const { getUserByEmail, findUsersByID, urlsForUser, equalShortURL, generateRandomString } = require("./helpers.js");
+const { users } = require("./server-data/user_info.js");
+const { urlDatabase } = require("./server-data/database.js");
 
 // after submitting the POST request, the data is sent as a buffer.
 // This line is to read that data.
@@ -17,50 +19,14 @@ app.use(cookieSession({
   maxAge: 24 * 60 * 60 * 1000 // 24 hours
 }));
 
-
 //tells the express app to use EJS as its templating engine
 app.set("view engine", "ejs");
 
-const urlDatabase = {
-  b2xVn2: {
-    longURL: "http://www.lighthouselabs.ca",
-    userID: "user1RandomID"
-  },
-  k3m5xK: {
-    longURL: "http://www.f1google.com",
-    userID: "user2RandomID",
-  },
-  k3msdK: {
-    longURL: "http://www.f2google.com",
-    userID: "user1RandomID",
-  },
-  k3masd: {
-    longURL: "http://www.f3google.com",
-    userID: "user2RandomID",
-  },
-};
-
-const users = {
-  user1RandomID: {
-    id: "user1RandomID",
-    email: "user@example.com",
-    password: "purple-monkey-dinosaur",
-  },
-  user2RandomID: {
-    id: "user2RandomID",
-    email: "user2@example.com",
-    password: "dishwasher-funk",
-  },
-};
 
 app.get("/", (req, res) => {
   return res.redirect("/urls");
 });
 
-// sends a message to conosle that server is running
-app.listen(PORT, () => {
-  return console.log(`Example app listening on port${PORT}!`);
-});
 
 app.get("/urls.json", (req, res) => {
   return res.json(urlDatabase);
@@ -68,21 +34,21 @@ app.get("/urls.json", (req, res) => {
 
 ////////////////////// a route for /urls
 app.get("/urls", (req, res) => {
-  const foundUserInfo = findUsersByID(req.session["user_id"]);
+  const foundUserInfo = findUsersByID(users,req.session["user_id"]);
   if (!foundUserInfo) {
     return res.end('<html><head><title>NOPE</title></head><body><h1>Please login or register</h1></body><form method="GET" action="/login"><button type="submit" class="btn btn-outline-primary">Login</button></form><form method="GET" action="/register"><button type="submit" class="btn btn-outline-primary">Register</button></form></html>');
   }
   // find the block of object
   const templateVars = {
     user: foundUserInfo,
-    urls: urlsForUser(foundUserInfo.id)
+    urls: urlsForUser(urlDatabase,foundUserInfo.id)
   };
   return res.render("urls_index", templateVars);
 });
 
 ////////////////////// adds a new route(page) to submit long url
 app.get("/urls/new", (req, res) => {
-  const foundUserInfo = findUsersByID(req.session["user_id"]);
+  const foundUserInfo = findUsersByID(users,req.session["user_id"]);
   // find the block of object
   const templateVars = {
     user: foundUserInfo
@@ -91,71 +57,6 @@ app.get("/urls/new", (req, res) => {
     return res.redirect("/login");
   }
   return res.render("urls_new", templateVars);
-});
-
-////////////////////// added a another route for /urls/:id; ":" tells that id is a route parameter
-app.get("/urls/:id", (req, res) => {
-  const foundUserInfo = findUsersByID(req.session["user_id"]);
-  // error message if not logged in
-  if (!foundUserInfo) {
-    return res.end('<html><head><title>NOPE</title></head><body><h1>Please login or register</h1></body><form method="GET" action="/login"><button type="submit" class="btn btn-outline-primary">login</button></form><form method="GET" action="/register"><button type="submit" class="btn btn-outline-primary">Register</button></form></html>');
-  }
-  // find the block of object
-  const templateVars = {
-    shortURL: req.params.id,
-    user: foundUserInfo,
-    urls: urlsForUser(foundUserInfo.id)
-  };
-  return res.render("urls_show", templateVars);
-});
-
-////////////////////// redirects to the longURL page when clicked
-app.get("/u/:id", (req, res) => {
-  const shortURL = req.params.id;
-  const longURL = urlDatabase[shortURL].longURL;
-  if (shortURL && longURL) {
-    return res.redirect(longURL);
-  }
-  return res.end(`The ${shortURL} does not exist`);
-});
-
-////////////////////// register route
-app.get("/register", (req, res) => {
-  const templateVars = {
-    user: req.session["user_id"]
-  };
-    // if the user is logged in, go to /urls otherwise to /register
-  if (templateVars.user) {
-    return res.redirect("/urls");
-  }
-  return res.render("register", templateVars);
-});
-
-////////////////////// a login route
-app.get("/login", (req, res) => {
-  const templateVars = {
-    user: req.session["user_id"]
-  };
-  // if the user is logged in, go to /urls otherwise to /login
-  if (templateVars.user) {
-    return res.redirect("/urls");
-  }
-  return res.render("login_page", templateVars);
-});
-
-
-//////POST///////POST/////POST///////POST/////////POST/////POST//////
-
-////////////////////// SIGN IN REQUEST
-app.post("/login", (req, res) => {
-  const foundUser = getUserByEmail(req.body.email, users);
-  // if encrypted input pass matches encrypted stored pass, go ahead.
-  if (bcrypt.compareSync(req.body.inputPassword, foundUser.password)) {
-    const userID = foundUser.id;
-    req.session.user_id = userID;
-    return res.redirect("/urls");
-  }
-  return res.send('403: Forbidden');
 });
 
 ////////////////////// CREATE A NEW SHORT URL FOR LONG URL REQUEST
@@ -172,10 +73,37 @@ app.post("/urls", (req, res) => {
   return res.redirect(`/urls/${shortURL}`);
 });
 
+
+////////////////////// added a another route for /urls/:id; ":" tells that id is a route parameter
+app.get("/urls/:id", (req, res) => {
+  const foundUserInfo = findUsersByID(users,req.session["user_id"]);
+  // error message if not logged in
+  if (!foundUserInfo) {
+    return res.end('<html><head><title>NOPE</title></head><body><h1>Please login or register</h1></body><form method="GET" action="/login"><button type="submit" class="btn btn-outline-primary">login</button></form><form method="GET" action="/register"><button type="submit" class="btn btn-outline-primary">Register</button></form></html>');
+  }
+  // find the block of object
+  const templateVars = {
+    shortURL: req.params.id,
+    user: foundUserInfo,
+    urls: urlsForUser(urlDatabase, foundUserInfo.id)
+  };
+  return res.render("urls_show", templateVars);
+});
+
+////////////////////// redirects to the longURL page when clicked
+app.get("/u/:id", (req, res) => {
+  const shortURL = req.params.id;
+  const longURL = urlDatabase[shortURL].longURL;
+  if (shortURL && longURL) {
+    return res.redirect(longURL);
+  }
+  return res.end(`The ${shortURL} does not exist`);
+});
+
 ////////////////////// EDIT REQUEST
 app.post("/urls/:id", (req, res) => {
   const id = req.params.id;
-  if (!req.session.user_id || equalShortURL(id)) {
+  if (!req.session.user_id || equalShortURL(urlDatabase ,id)) {
     return res.end('You cannot create new shortened URL if you are not logged in.');
   }
   urlDatabase[id] = {
@@ -188,17 +116,24 @@ app.post("/urls/:id", (req, res) => {
 ////////////////////// DELTE REQUEST
 app.post("/urls/:id/delete", (req, res) => {
   const shortURLID = req.params.id;
-  if (!req.session.user_id || equalShortURL(shortURLID)) {
+  if (!req.session.user_id || equalShortURL(urlDatabase, shortURLID)) {
     return res.end('Error. You are trying to delete an URL that does not exist or that you are not signed in.');
   }
   delete urlDatabase[shortURLID];
   return res.redirect("/urls");
 });
 
-////////////////////// LOGOUT REQUEST
-app.post("/logout", (req, res) => {
-  req.session = null;
-  return res.redirect("/login");
+
+////////////////////// register route
+app.get("/register", (req, res) => {
+  const templateVars = {
+    user: req.session["user_id"]
+  };
+  // if the user is logged in, go to /urls otherwise to /register
+  if (templateVars.user) {
+    return res.redirect("/urls");
+  }
+  return res.render("register", templateVars);
 });
 
 ////////////////////// REGISTER REQUEST
@@ -226,37 +161,39 @@ app.post("/register", (req, res) => {
   return res.redirect("/urls");
 });
 
-const generateRandomString = () => {
-  let randomString = "";
-  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-  const numbers = "1234567890";
-  const alphaNumbers = alphabet + numbers;
-  randomString += alphabet[Math.floor(Math.random() * alphabet.length)];
-  for (let i = 0; i < 5; i++) {
-    randomString += alphaNumbers[Math.floor(Math.random() * alphaNumbers.length)];
-  }
-  return randomString;
-};
 
-const findUsersByID = (userID) => {
-  return users[userID];
-};
-
-const urlsForUser = (userID) => {
-  const filteredURL = {};
-  for (const shortURL in urlDatabase) {
-    if (urlDatabase[shortURL]["userID"] === userID) {
-      filteredURL[shortURL] = urlDatabase[shortURL];
-    }
+////////////////////// a login route
+app.get("/login", (req, res) => {
+  const templateVars = {
+    user: req.session["user_id"]
+  };
+  // if the user is logged in, go to /urls otherwise to /login
+  if (templateVars.user) {
+    return res.redirect("/urls");
   }
-  return filteredURL;
-};
+  return res.render("login_page", templateVars);
+});
 
-const equalShortURL = (shortURL) => {
-  for (const shortURL in urlDatabase) {
-    if (urlDatabase[shortURL] === shortURL) {
-      return true;
-    }
+////////////////////// SIGN IN REQUEST
+app.post("/login", (req, res) => {
+  const foundUser = getUserByEmail(req.body.email, users);
+  // if encrypted input pass matches encrypted stored pass, go ahead.
+  if (bcrypt.compareSync(req.body.inputPassword, foundUser.password)) {
+    const userID = foundUser.id;
+    req.session.user_id = userID;
+    return res.redirect("/urls");
   }
-  return false;
-};
+  return res.send('403: Forbidden');
+});
+
+////////////////////// LOGOUT REQUEST
+app.post("/logout", (req, res) => {
+  req.session = null;
+  return res.redirect("/login");
+});
+
+
+// sends a message to conosle that server is running
+app.listen(PORT, () => {
+  return console.log(`Example app listening on port${PORT}!`);
+});
